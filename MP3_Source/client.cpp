18c46fd2b56937f32fc3a3c26ca105317f1647b3
Ryan Parker <ryan@ryanparker.org>
2020-10-17 14:00:24 -0500

    Finished step 3

--- MP3_Source/client.cpp
+++ MP3_Source/client.cpp
@@ -1,19 +1,19 @@
 /* 
-    File: client.cpp
+  File: client.cpp
 
-    Author: R. Parkekr
-            Department of Computer Science
-            Texas A&M University
-    Date  : 2020/09/22
+  Author: R. Parkekr
+      Department of Computer Science
+      Texas A&M University
+  Date  : 2020/09/22
 
-    Client main program for MP2 in CSCE 313
+  Client main program for MP2 in CSCE 313
 */
 
 /*--------------------------------------------------------------------------*/
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
 
-    /* -- (none) -- */
+  /* -- (none) -- */
 
 /*--------------------------------------------------------------------------*/
 /* INCLUDES */
@@ -32,24 +32,41 @@
 #include <unistd.h>
 
 #include "reqchannel.hpp"
+#include "pc_buffer.hpp"
 
 /*--------------------------------------------------------------------------*/
 /* DATA STRUCTURES */ 
 /*--------------------------------------------------------------------------*/
+typedef struct RTFArgs{
+  int n_reqs;
+  string patient_name;
+  PCBuffer * buffer;
+} RTFArgs;
 
-    /* -- (none) -- */
+typedef struct WTFArgs{
+  PCBuffer * buffer;
+  RequestChannel * rc;
+  int thread_id;
+} WTFArgs;
+
+//PCBuffer buffer;
+
+// defaults for command line arguments
+int bufferSize = 500;
+int numWorkerThread = 10;
+int numRequests = 10000;
 
 /*--------------------------------------------------------------------------*/
 /* CONSTANTS */
 /*--------------------------------------------------------------------------*/
 
-    /* -- (none) -- */
+  /* -- (none) -- */
 
 /*--------------------------------------------------------------------------*/
 /* FORWARDS */
 /*--------------------------------------------------------------------------*/
 
-    /* -- (none) -- */
+  /* -- (none) -- */
 
 /*--------------------------------------------------------------------------*/
 /* LOCAL FUNCTIONS -- SUPPORT FUNCTIONS */
@@ -62,25 +79,51 @@ std::string int2string(int number) {
 }
 
 void print_time_diff(const std::string _label,
-                     const struct timeval & _tp1,
-                     const struct timeval & _tp2) {
-    // Prints to stdout the difference, in seconds and museconds, between
-    // two timevals.
-    
-    std::cout << _label;
-    long sec = _tp2.tv_sec - _tp1.tv_sec;
-    long musec = _tp2.tv_usec - _tp1.tv_usec;
-    if (musec < 0) {
-        musec += 1000000;
-        sec--;
-    }
-    std::cout << " [sec = " << sec << ", musec = " << musec << "]" << std::endl;
-    
+           const struct timeval & _tp1,
+           const struct timeval & _tp2) {
+  // Prints to stdout the difference, in seconds and museconds, between
+  // two timevals.
+  
+  std::cout << _label;
+  long sec = _tp2.tv_sec - _tp1.tv_sec;
+  long musec = _tp2.tv_usec - _tp1.tv_usec;
+  if (musec < 0) {
+    musec += 1000000;
+    sec--;
+  }
+  std::cout << " [sec = " << sec << ", musec = " << musec << "]" << std::endl;
+  
 }
 
 std::string generate_data() {
   // Generate the data to be returned to the client.
   return int2string(rand() % 100);
+}
+
+void * request_thread(void * args) {
+  RTFArgs * rtfa = (RTFArgs *) args;
+  //cout << "Patient name: " << rtfa->patient_name << endl;
+  for(int i = 0; i < rtfa->n_reqs; i++) {
+    string req = "data " + rtfa->patient_name;
+    //cout << "Requesting: " << req << endl;
+    rtfa->buffer->Deposit(req);
+  }
+  rtfa->buffer->Deposit("Done");
+}
+
+void * worker_thread(void * args) {
+  WTFArgs * wtfa = (WTFArgs *) args;
+  for(;;) {
+    string req = wtfa->buffer->Retrieve();
+    if(req == "Done") {
+      delete wtfa->rc;
+      break;
+    }
+    wtfa->rc->send_request(req);
+    cout << "req = " << req << endl;
+  }
+
+  //if
 }
 
 /*--------------------------------------------------------------------------*/
@@ -89,17 +132,37 @@ std::string generate_data() {
 
 int main(int argc, char * argv[]) {
 
+  //Handle 
+  int cmd;
+  while((cmd = getopt(argc, argv, "b:n:w:")) != -1) {
+    switch(cmd) {
+      case 'b':
+        bufferSize = atoi(optarg);
+        break;
+      case 'n':
+        numRequests = atoi(optarg);
+        break;
+      case 'w':
+        numWorkerThread = atoi(optarg);
+        break;
+      case '?':
+        cout << "Invalid argument aborting" << endl << endl;
+        return 1;
+      default:
+        cout << "Command line error" << endl;
+        cout << "Options:\n-b=# : Size of PCBuffer\n-n=# : Number of requests\n-w=# : Number of worker threads" << endl;
+        return 1;
+    }
+  }
+
   if(fork() == 0) {
     //char * args[]={"./dataserver", nullptr};
     //execvp(args[0], args);
     execvp("./dataserver", NULL);
-    std::cout << "test" << std::endl;
   }
 
-
-
-
-
+  //cout << bufferSize << endl << endl << endl;
+  //buffer = new PCBuffer(bufferSize);
 
 
   std::cout << "CLIENT STARTED:" << std::endl;
@@ -108,9 +171,56 @@ int main(int argc, char * argv[]) {
   RequestChannel chan("control", RequestChannel::Side::CLIENT);
   std::cout << "done." << std::endl;
 
+  //PCBuffer * buffer = new PCBuffer(bufferSize);
+  PCBuffer buffer(bufferSize);
+  pthread_t requestThread[3];
+  pthread_t workerThread[numWorkerThread];
+
+  WTFArgs workerData[numWorkerThread];
+
+  
+  std::cout << "Create worker threads" << std::endl;
+  for(int i = 0; i < numWorkerThread; i++) {
+    workerData[i].buffer = &buffer;
+    workerData[i].thread_id = i;
+
+    string reply5 = chan.send_request("newthread");
+    cout << "Reply to request 'newthread' is " << reply5 << "" << endl;
+    workerData[i].rc = new RequestChannel(reply5, RequestChannel::Side::CLIENT);
+    pthread_create(&workerThread[i], NULL, worker_thread, (void *)&workerData[i]);
+  }
+
+
+  cout << "Create request threads" << endl;
+  RTFArgs requestData[3];
+  requestData[0].n_reqs = numRequests;
+  requestData[0].patient_name = "Joe Smith";
+  requestData[0].buffer = &buffer;
+  requestData[1].n_reqs = numRequests;
+  requestData[1].patient_name = "Jane Smith";
+  requestData[1].buffer = &buffer;
+  requestData[2].n_reqs = numRequests;
+  requestData[2].patient_name = "John Smith";
+  requestData[2].buffer = &buffer;
+  
+  pthread_create(&requestThread[0], NULL, request_thread, (void*)&requestData[0]);
+  pthread_create(&requestThread[1], NULL, request_thread, (void*)&requestData[1]);
+  pthread_create(&requestThread[2], NULL, request_thread, (void*)&requestData[2]);
+
+  for(int i = 0; i < 3; i++) {
+    pthread_join(requestThread[i], NULL);
+  }
+
+  for(int i = 0; i < numWorkerThread; i++) {
+    pthread_join(workerThread[i], NULL);
+  }
+
+  //cout << "test" << endl;
+
+
   /* -- Start sending a sequence of requests */
 
-  std::string reply1 = chan.send_request("hello");
+  /*std::string reply1 = chan.send_request("hello");
   std::cout << "Reply to request 'hello' is '" << reply1 << "'" << std::endl;
 
   std::string reply2 = chan.send_request("data Joe Smith");
@@ -127,9 +237,9 @@ int main(int argc, char * argv[]) {
   assert(gettimeofday(&timeEnd_1, 0) == 0);
 
   for(int i = 0; i < 100; i++) {
-    std::string request_string("data TestPerson" + int2string(i));
-    std::string reply_string = chan.send_request(request_string);
-    std::cout << "reply to request " << i << ":" << reply_string << std::endl;;
+  std::string request_string("data TestPerson" + int2string(i));
+  std::string reply_string = chan.send_request(request_string);
+  std::cout << "reply to request " << i << ":" << reply_string << std::endl;;
   }
 
   struct timeval timeStart_2;
@@ -147,6 +257,6 @@ int main(int argc, char * argv[]) {
 
   print_time_diff("Time taken for computation remotely: ", timeStart_1, timeEnd_1);
   print_time_diff("Time taken for computation localy: ", timeStart_2, timeEnd_2);
-
+*/
   usleep(1000000);
 }
